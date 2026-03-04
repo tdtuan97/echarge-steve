a2838eb25d2741d002a63bbefb489dd76448676c
tran.tuan <tdtuan97@gmail.com>
2026-03-04 20:42:06 +0700

    update metervalie

--- src/main/java/de/rwth/idsg/steve/config/BeanConfiguration.java
+++ src/main/java/de/rwth/idsg/steve/config/BeanConfiguration.java
@@ -134,7 +134,7 @@ public class BeanConfiguration implements WebMvcConfigurer {
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         //scheduler.setPoolSize(10);
-        scheduler.setPoolSize(20);
+        scheduler.setPoolSize(100);
         scheduler.setThreadNamePrefix("SteVe-TaskScheduler-");
         scheduler.setWaitForTasksToCompleteOnShutdown(true);
         scheduler.setAwaitTerminationSeconds(30);
